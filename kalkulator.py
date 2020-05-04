48aab7d35a4568ac9bd51092b41d97995db48c1e
voss-m <michelmaciejvoss@gmail.com>
2020-05-04 18:48:35 +0200

    Dodana funckja get_info

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,11 @@
+
+def get_info():
+	print('Witaj, prosty kalkulator')
+
 def dodaj():
 	print('Wprowadz dwie liczby a zostana dodane')
 	a = int(input())
 	b = int(input())
 	print(a+b)
+get_info()
 dodaj()
